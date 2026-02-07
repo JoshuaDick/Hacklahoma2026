06bd0402a325b3080881497134f92f8c64c17e28
snowie-ctrl <smokingsnake55@gmail.com>
2026-02-07 12:35:30 -0600

    Motor pins defined

---
+++ ESP32/code/code.ino
@@ -0,0 +1,33 @@
+
+  // Front Left Wheel
+#define int FLtopLeft 1;
+#define int FLtopRight 2;
+#define int FLbottomLeft 3;
+#define int FLbottomRight 4;
+  // Front Right Wheel
+#define int FRtopLeft 5;
+#define int FRtopRight 6;
+#define int FRbottomLeft 7;
+#define int FRbottomRight 8;
+  // Back Left Wheel
+#define int BLtopLeft 9;
+#define int BLtopRight 10;
+#define int BLbottomLeft 11;
+#define int BLbottomRight 12;
+  // Back Right Wheel
+#define int BRtopLeft 13;
+#define int BRtopRight 14;
+#define int BRbottomLeft 15;
+#define int BRbottomRight 16;
+
+
+
+
+setup() {
+  
+}
+
+
+loop() {
+
+}
