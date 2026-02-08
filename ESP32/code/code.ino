5a891aa3c6ed336786d4631a2acc86938f84a753
snowie-ctrl <smokingsnake55@gmail.com>
2026-02-07 20:06:23 -0600

    Full driving commands
    // This program controls the h-bridge for each motor of our RC car (1 h-bridge per motor). It recieves commands via
    // a bluetooth signal that is connected to a mobile phone. There are preset commands that the user needs to enter to
    // remotely change the behavior of the motors, thus altering the way the car drives. When the command is processed, it
    // activates the "mode" it commanded and keeps it on, so if the user commands "forward", it will keep going forward
    // until another valid command is recieved. Thus, overriding the previous behavior.
    
    valid commands:
    forward, backward, idle
    
    if wanting to control specific wheels, use 4 letters with no spaces:
    f -> forward for that wheel
    b -> backwards for that wheel
    i -> idle that wheel
    
    index corresponds to wheel in this way:
    0 1
    2 3

--- ESP32/code/code.ino
+++ ESP32/code/code.ino
@@ -1,8 +1,17 @@
 
 
-#include "bluetooth.h"
-#
+// This program controls the h-bridge for each motor of our RC car (1 h-bridge per motor). It recieves commands via
+// a bluetooth signal that is connected to a mobile phone. There are preset commands that the user needs to enter to
+// remotely change the behavior of the motors, thus altering the way the car drives. When the command is processed, it
+// activates the "mode" it commanded and keeps it on, so if the user commands "forward", it will keep going forward
+// until another valid command is recieved. Thus, overriding the previous behavior.
 
+
+
+
+
+
+#include "bluetooth.h"
 
   // =============== Structures ===============
 struct MotorPins {
@@ -44,13 +53,9 @@ Direction commandToDirection(char c) {
     case 'f': return FORWARD;
     case 'b': return BACKWARD;
     case 'i': return IDLE;
-    case 's': return STOP;
   }
 }
 
-
-
-const char* COMMAND_DELIMITER = " ";
 
  // ===========================================
 
@@ -58,37 +63,64 @@ const char* COMMAND_DELIMITER = " ";
 
  // ================ FUNCTIONS ================
 
+
+
+
+  
+ // ===========================================
+
+
+
+ // ============ DRIVE FUNCTIONS ==============
+
 void driveWheel(Wheel wheel, Direction dir) {
   MotorPins m = motors[wheel];
 
   switch (dir) {
     case FORWARD:
-      digitalWrite(m.IN1, HIGH);
-      digitalWrite(m.IN2, LOW);
-      digitalWrite(m.IN3, HIGH);
-      digitalWrite(m.IN4, LOW);
-      break;
-
-    case BACKWARD:
       digitalWrite(m.IN1, LOW);
       digitalWrite(m.IN2, HIGH);
       digitalWrite(m.IN3, LOW);
       digitalWrite(m.IN4, HIGH);
       break;
 
-    case IDLE:
+    case BACKWARD:
+      digitalWrite(m.IN1, HIGH);
+      digitalWrite(m.IN2, LOW);
+      digitalWrite(m.IN3, HIGH);
+      digitalWrite(m.IN4, LOW);
       break;
 
-    default:
-      digitalWrite(m.IN1, LOW);
-      digitalWrite(m.IN2, LOW);
+    case IDLE:
+      digitalWrite(m.IN1, HIGH);
+      digitalWrite(m.IN2, HIGH);
       digitalWrite(m.IN3, LOW);
       digitalWrite(m.IN4, LOW);
       break;
-    };
   }
 
-  
+void driveForward() {
+  driveWheel(FRONT_LEFT, FORWARD);
+  driveWheel(FRONT_RIGHT, FORWARD);
+  driveWheel(BACK_LEFT, FORWARD);
+  driveWheel(BACK_RIGHT, FORWARD);
+}
+
+void driveBackward() {
+  driveWheel(FRONT_LEFT, BACKWARD);
+  driveWheel(FRONT_RIGHT, BACKWARD);
+  driveWheel(BACK_LEFT, BACKWARD);
+  driveWheel(BACK_RIGHT, BACKWARD);
+}
+
+void driveIdle() {
+  driveWheel(FRONT_LEFT, IDLE);
+  driveWheel(FRONT_RIGHT, IDLE);
+  driveWheel(BACK_LEFT, IDLE);
+  driveWheel(BACK_RIGHT, IDLE);
+}
+
+
  // ===========================================
 
 
@@ -107,8 +139,8 @@ void setup() {
     pinMode(motors[i].IN3, OUTPUT);
     pinMode(motors[i].IN4, OUTPUT);
 
-    digitalWrite(motors[i].IN1, LOW);
-    digitalWrite(motors[i].IN2, LOW);
+    digitalWrite(motors[i].IN1, HIGH);
+    digitalWrite(motors[i].IN2, HIGH);
     digitalWrite(motors[i].IN3, LOW);
     digitalWrite(motors[i].IN4, LOW);
   }
@@ -128,9 +160,18 @@ void loop() {
   Serial.println("Command: " + command);
 
   if (!command.equals("none")) {
-    if (command.length() != 4 && ) { // generalized command
+    if (command.length() != 4) { // generalized command
       Serial.println("General command: " + command);
+      if (command == "forward") {
+        driveForward();
+      } else if (command == "backward") {
+        driveBackward();
+      } else if (command == "idle") {
+        driveIdle();
+      }
+
     } else { // Wheel specific commands
+      Serial.println("Custom command");
       driveWheel(FRONT_LEFT, commandToDirection(command.charAt(0)));
       driveWheel(FRONT_RIGHT, commandToDirection(command.charAt(1)));
       driveWheel(BACK_LEFT, commandToDirection(command.charAt(2)));
